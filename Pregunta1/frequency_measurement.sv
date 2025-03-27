dd842e1b88311c3d774907ee3c2b5204560d34d6
AdrMuAl <131110706+AdrMuAl@users.noreply.github.com>
2025-03-27 02:18:09 -0600

    def de señales internas

--- Pregunta1/frequency_measurement.sv
+++ Pregunta1/frequency_measurement.sv
@@ -7,4 +7,10 @@ module frequency_measurement #(
     input logic [3:0] opcode,       // Código de operación para la ALU
     output logic [WIDTH-1:0] result,// Resultado de la ALU
     output logic N, Z, C, V         // Banderas de la ALU
+
+        // Señales internas
+    logic [WIDTH-1:0] reg_a, reg_b;
+    logic [WIDTH-1:0] alu_result;
+    logic alu_N, alu_Z, alu_C, alu_V;
+
 );
